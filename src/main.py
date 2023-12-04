c8044203f9acd5ee367963d9a94a5bb74c5c767f
estem8 <awards@mail.ru>
2023-12-04 13:57:21 +0300

    Создать render_template и базовый CSS Fixes #3

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'Test string'
-
+    return render_template('index.html',data = 'test_string')
 
 if __name__=='__main__':
     app.run(debug=True)
