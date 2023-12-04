d9d77896c1aaf42232142fad318c4d35db164fc7
Stepan S <48146777+estem8@users.noreply.github.com>
2023-12-04 20:00:52 +0300

    Разобраться что написал Корнеев О.о (#13)

--- src/models.py
+++ src/models.py
@@ -1,18 +1,24 @@
-from sqlalchemy import Column, Integer, String
+"""
+manual
+https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/basic_use.html
 
-from db import Base, engine
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from db import engine
+
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True)
     name = Column(String())
-    s_name = Column(String())
+    password = Column(String())
+    login = Column(String())
     email = Column(String(120), unique=True)
 
     def __repr__(self):
         return f"User {self.id}, {self.name}"
 
-
 if __name__ == "__main__":
-    Base.metadata.create_all(bind=engine)
+     Base.metadata.create_all(engine)
