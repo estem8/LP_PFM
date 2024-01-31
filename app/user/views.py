c43e8390f9b8b707bdb8b2a132aa747c1fee2e55
Mikhail Artemov <babanlive@gmail.com>
2024-02-01 03:01:37 +0700

    Добавил страницу дашборда. Осталось сделать подсчет баланса

--- app/user/views.py
+++ app/user/views.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user,login_required, login_user, logout_user
+from sqlalchemy import select
 
 from app import Session
 from app.crud import new_user
-from app.models import User
+from app.models import Account, User
 from app.user.forms import LoginForm, RegistrationForm
 
 
@@ -22,12 +23,12 @@ def login():
 @blueprint.route('/signup', methods=['GET', 'POST'], endpoint='signup')
 def signup():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('user.dashboard'))
     title = 'Регистрация'
     reg_form = RegistrationForm()
     if request.method == 'POST' and reg_form.validate():
         new_user(reg_form.data)
-        return redirect(url_for('index'))
+        return redirect(url_for('user.login'))
     return render_template('user/signup.html', page_title=title, form=reg_form)
 
 
@@ -40,7 +41,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно авторизовались')
-            return redirect(url_for('index'))
+            return redirect(url_for('user.dashboard'))
     flash('Неверный логин или пароль')
     return redirect(url_for('user.login'))
 
@@ -50,3 +51,12 @@ def logout():
     logout_user()
     flash('Вы успешно разлогинились')
     return redirect(url_for('index'))
+
+
+@blueprint.route('/dashboard')
+@login_required
+def dashboard():
+    with Session() as session:
+        query = select(Account).where(Account.user_id == current_user.id)
+        accounts = session.execute(query).scalars().all()
+        return render_template('user/dashboard.html', accounts=accounts)
